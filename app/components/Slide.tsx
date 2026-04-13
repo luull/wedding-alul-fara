c29dfbcf03ee521ac96fbf21099044170826dd51
luull <59054817+luull@users.noreply.github.com>
2026-04-13 19:43:25 +0700

    init

---
+++ app/components/Slide.tsx
@@ -0,0 +1,43 @@
+type SlideProps = {
+  image: string;
+  children?: React.ReactNode;
+};
+
+export default function Slide({ image, children }: SlideProps) {
+  return (
+    <section
+      style={{
+        height: "auto",
+        width: "100%",
+        position: "relative",
+        scrollSnapAlign: "start",
+        backgroundColor: "#f8f4f3",
+      }}
+    >
+      <img
+        src={image}
+        alt="slide"
+        style={{
+          width: "100%",
+          height: "100%",
+          objectFit: "contain",
+        }}
+      />
+
+      {/* Overlay */}
+      <div
+        style={{
+          position: "absolute",
+          inset: 0,
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          flexDirection: "column",
+          pointerEvents: "none",
+        }}
+      >
+        <div style={{ pointerEvents: "auto" }}>{children}</div>
+      </div>
+    </section>
+  );
+}
