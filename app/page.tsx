cbe107528fa80ffda0f43107cf9dad94c56b06f0
luull <59054817+luull@users.noreply.github.com>
2026-04-15 21:49:19 +0700

    fix responsive

--- app/page.tsx
+++ app/page.tsx
@@ -22,60 +22,62 @@ function Content() {
   };
 
   return (
-    <main
-      style={{
-        height: "100vh",
-        overflowY: "scroll",
-        scrollSnapType: "y mandatory",
-      }}
-    >
-      <Slide image="/images/slide1.png">
-        <p
-          style={{
-            fontSize: "22px",
-            fontWeight: 600,
-            color: "#000",
-            marginTop: "550px",
-            textAlign: "center",
-          }}
-        >
-          {guestName}
-        </p>
+    <div style={outerWrapper}>
+      <div style={phoneWrapper}>
+        <main style={innerWrapper}>
+          <Slide image="/images/slide1.png">
+            <p
+              style={{
+                fontSize: "22px",
+                fontWeight: 600,
+                color: "#000",
+                marginTop: "550px",
+                textAlign: "center",
+              }}
+            >
+              {guestName}
+            </p>
 
-        <h2
-          style={{ textAlign: "center", fontSize: "16px", color: "#7d7d7dff" }}
-        >
-          you are invited to our wedding
-        </h2>
-      </Slide>
+            <h2
+              style={{
+                textAlign: "center",
+                fontSize: "16px",
+                color: "#7d7d7dff",
+              }}
+            >
+              you are invited to our wedding
+            </h2>
+          </Slide>
 
-      <Slide image="/images/slide2.png" />
-      <Slide image="/images/slide3.png">
-        <MusicPlayer />
-      </Slide>
-      <Slide image="/images/slide4.png" />
+          <Slide image="/images/slide2.png" />
+          <Slide image="/images/slide3.png">
+            <MusicPlayer />
+          </Slide>
+          <Slide image="/images/slide4.png" />
 
-      <Slide image="/images/slide5.png">
-        <a
-          href="https://maps.app.goo.gl/zRv5wuhWT2aSQ2qR7"
-          target="_blank"
-          style={btnStyle2}
-        >
-          Show Location
-        </a>
-      </Slide>
+          <Slide image="/images/slide5.png">
+            <a
+              href="https://maps.app.goo.gl/zRv5wuhWT2aSQ2qR7"
+              target="_blank"
+              style={btnStyle2}
+            >
+              Show Location
+            </a>
+          </Slide>
 
-      <Slide image="/images/slide6.png" />
+          <Slide image="/images/slide6.png" />
 
-      <Slide image="/images/slide7.png">
-        <button onClick={handleCopy} style={btnStyle4}>
-          {copied ? <Check size={18} /> : <Copy size={18} />}
-        </button>
-      </Slide>
+          <Slide image="/images/slide7.png">
+            <button onClick={handleCopy} style={btnStyle4}>
+              {copied ? <Check size={18} /> : <Copy size={18} />}
+            </button>
+          </Slide>
 
-      <Slide image="/images/slide8.png" />
-      <Toaster position="top-center" />
-    </main>
+          <Slide image="/images/slide8.png" />
+          <Toaster position="top-center" />
+        </main>
+      </div>
+    </div>
   );
 }
 
@@ -92,8 +94,10 @@ const btnStyle2: React.CSSProperties = {
   color: "#fff",
   padding: "14px 20px",
   borderRadius: "12px",
-  marginTop: "480px",
   fontSize: "16px",
+  position: "absolute",
+  marginTop: "200px",
+  right: "120px",
   cursor: "pointer",
 };
 
@@ -106,4 +110,24 @@ const btnStyle4: React.CSSProperties = {
   marginTop: "100px",
   marginLeft: "20px",
   cursor: "pointer",
+};
+
+const outerWrapper: React.CSSProperties = {
+  display: "flex",
+  justifyContent: "center",
+  background: "#f8f4f3",
+};
+
+const phoneWrapper: React.CSSProperties = {
+  transform: "scale(0.85)",
+  transformOrigin: "center",
+};
+
+const innerWrapper: React.CSSProperties = {
+  width: "100%",
+  maxWidth: "420px",
+  height: "100vh",
+  overflowY: "scroll",
+  scrollSnapType: "y mandatory",
+  background: "#fff",
 };
